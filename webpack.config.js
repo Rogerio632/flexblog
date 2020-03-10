de717edc6a19a472cdadb6ed655e7dddcee97618
Rogério De Oliveira <rogeriofilho632@gmail.com>
2020-03-10 15:26:08 -0300

    feat: add css 'n style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  entry:path.resolve(__dirname, 'src', 'index.js'),
   
-  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
@@ -12,13 +12,19 @@ module.exports = {
   },
   module: {
     rules: [
-        {
+      {
         test: '/\*.js$/',
         exclude: '/node_modules/',
         use: {
           loader: 'babel-loader',
         },
       },
+      {
+        test: '/\*.css$/',
+        use: {
+          loader: ['css-loader', 'style-loader'],
+        },
+      },
         ],
-  },
+},
 };
